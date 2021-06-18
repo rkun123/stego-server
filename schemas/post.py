5919ef2cdaee32173a54609db9b2ec5f33b9001b
PigeonsHouse <bullbull1230@gmail.com>
2021-06-18 23:30:36 +0900

    [fix] postモデルの修正

--- schemas/post.py
+++ schemas/post.py
@@ -1,20 +1,20 @@
-from typing import List, String, Optional, Union
+from typing import List, Optional, Union
 from datetime import datetime, timedelta
 from pydantic import BaseModel, Field
 from .user import BaseUser, User
 
 class BasePost(BaseModel):
-	content: String = Field()
-	content_length: float = Field()
-	user_id: String = Field()
-	reply_to_id: Optional[String] = Field()
+	content: str = Field()
+	content_length: int = Field()
+	user_id: str = Field()
+	reply_to_id: Optional[str] = Field()
 	writing_time: timedelta = Field()
 	lat: Optional[float] = Field()
 	lng: Optional[float] = Field()
 	elevation: Optional[float] = Field()
 	velocity: Optional[float] = Field()
 	direction: Optional[float] = Field()
-	weathre: Optional[String] = Field()
+	weather: Optional[str] = Field()
 	gyro_x: Optional[float] = Field()
 	gyro_y: Optional[float] = Field()
 	gyro_z: Optional[float] = Field()
@@ -26,6 +26,6 @@ class BasePost(BaseModel):
 		orm_mode = True
 
 class Post(BasePost):
-	id: String = Field()
+	id: str = Field()
 	created_at: datetime = Field(default_factory=datetime.now)
 	updated_at: datetime = Field(default_factory=datetime.now)
