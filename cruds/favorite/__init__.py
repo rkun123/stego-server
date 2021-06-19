754f202b9da1856b0b48fab52d21f81db0cc1a40
PigeonsHouse <bullbull1230@gmail.com>
2021-06-19 13:51:28 +0900

    [fix] HTTPエラーのエラー文修正

--- cruds/favorite/__init__.py
+++ cruds/favorite/__init__.py
@@ -8,9 +8,9 @@ from starlette.exceptions import HTTPException
 
 def favoriting_post(db: Session, post_id: str, user_id: str) -> Favorite:
 	if get_post_by_id(db, post_id) == None:
-		raise HTTPException(400, 'No such post')
+		raise HTTPException(404, 'Post not found')
 	if get_user_by_id(db, user_id) == None:
-		raise HTTPException(400, 'No such user')
+		raise HTTPException(404, 'User not found')
 	if db.query(models.Favorite).filter(models.Favorite.post_id == post_id, models.Favorite.user_id == user_id).count():
 		raise HTTPException(400, 'Already favorite the post')
 
@@ -24,12 +24,12 @@ def favoriting_post(db: Session, post_id: str, user_id: str) -> Favorite:
 
 def unfavoriting_post(db: Session, post_id: str, user_id: str) -> None:
 	if get_post_by_id(db, post_id) == None:
-		raise HTTPException(400, 'No such post')
+		raise HTTPException(404, 'Post not found')
 	if get_user_by_id(db, user_id) == None:
-		raise HTTPException(400, 'No such user')
+		raise HTTPException(404, 'User not found')
 	favorite_info = db.query(models.Favorite).filter(models.Favorite.post_id == post_id, models.Favorite.user_id == user_id).first()
 	if favorite_info == None:
-		raise HTTPException(400, 'Already unfavorite the post')
+		raise HTTPException(400, 'You don\'t favorite the post')
 	db.delete(favorite_info)
 	db.commit()
 
