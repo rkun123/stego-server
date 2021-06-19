517f16c5c6e966c8e7bce48a181ed69306dd45e5
PigeonsHouse <bullbull1230@gmail.com>
2021-06-19 16:27:04 +0900

    [add] 投稿用のスキーマ作成

--- schemas/post.py
+++ schemas/post.py
@@ -31,3 +31,22 @@ class Post(BasePost):
 	id: str = Field()
 	created_at: datetime = Field(default_factory=datetime.now)
 	updated_at: datetime = Field(default_factory=datetime.now)
+
+class CreatePost(BaseModel):
+	content: str = Field()
+	reply_to_id: Optional[str] = Field()
+	writing_time: timedelta = Field()
+	lat: Optional[float] = Field()
+	lng: Optional[float] = Field()
+	elevation: Optional[float] = Field()
+	velocity: Optional[float] = Field()
+	direction: Optional[float] = Field()
+	weather: Optional[str] = Field()
+	gyro_x: Optional[float] = Field()
+	gyro_y: Optional[float] = Field()
+	gyro_z: Optional[float] = Field()
+
+	images: List[Image] = Field()
+
+	class Config:
+		orm_mode = True
