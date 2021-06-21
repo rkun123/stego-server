7721724c32dc835f03412d4dd6b285ab0876e7dc
PigeonsHouse <bullbull1230@gmail.com>
2021-06-21 21:24:45 +0900

    [Fix] ソート，フィルターのバグ修正と標準ソートの変更

--- cruds/posts/__init__.py
+++ cruds/posts/__init__.py
@@ -1,20 +1,16 @@
-from re import template
 import re
+from sqlalchemy.sql.expression import desc
 from cruds.users.auth import get_current_user
 from cruds.image import upload_image
-from os import name
-from routers import posts
-from sqlalchemy.sql.elements import Null, and_, or_
+from sqlalchemy.sql.elements import or_
 from schemas.user import User
 from schemas.post import BasePost, MaxMin, Post, QueryPost
 from schemas.image import Image
-from cruds.users import get_user_by_id
 from typing import List, Optional, Set
 from starlette.exceptions import HTTPException
 from db import base, schemas as models
 from sqlalchemy.orm import Session
 from datetime import datetime, timedelta
-import hashlib
 import os
 import requests
 
@@ -22,58 +18,62 @@ weather_api_key = os.environ.get('WEATHER_API_KEY')
 base_url = "http://api.openweathermap.org/data/2.5/forecast"
 
 def get_timeline(db: Session) -> List[Post]:
-	posts = db.query(models.Post).all()
+	posts = db.query(models.Post).order_by(desc(models.Post.created_at)).all()
 	return list(map(Post.from_orm, posts))
 
 def get_query_timeline(db: Session, request: QueryPost, user: User) -> List[Post]:
 	query_timeline = db.query(models.Post)
-	if request.filter.temperture:
-		query_timeline = query_timeline.filter(models.Post.tempurture != None)
-		if request.filter.temperture.max:
-			query_timeline = query_timeline.filter(models.Post.tempurture < request.filter.temperture.max)
-		if request.filter.temperture.min:
-			query_timeline = query_timeline.filter(models.Post.tempurture >= request.filter.temperture.min)
-	if request.filter.velocity:
-		query_timeline = query_timeline.filter(models.Post.velocity != None)
-		if request.filter.velocity.max:
-			query_timeline = query_timeline.filter(models.Post.velocity < request.filter.velocity.max)
-		if request.filter.velocity.min:
-			query_timeline = query_timeline.filter(models.Post.velocity >= request.filter.velocity.min)
-	if request.filter.elevation:
-		query_timeline = query_timeline.filter(models.Post.elevation != None)
-		if request.filter.elevation.max:
-			query_timeline = query_timeline.filter(models.Post.elevation < request.filter.elevation.max)
-		if request.filter.elevation.min:
-			query_timeline = query_timeline.filter(models.Post.elevation >= request.filter.elevation.min)
-	if request.filter.direction:
-		query_timeline = query_timeline.filter(models.Post.gyro_x != None).filter(models.Post.gyro_y != None).filter(models.Post.gyro_z != None)
-		if request.filter.direction == "up":
-			query_timeline = query_timeline.filter(-30 < models.Post.gyro_x, models.Post.gyro_x < 30, -30 < models.Post.gyro_y, models.Post.gyro_y < 30)
-		if request.filter.direction == "down":
-			query_timeline = query_timeline.filter(or_(150 < models.Post.gyro_x, models.Post.gyro_x < -150)).filter(-30 < models.Post.gyro_y, models.Post.gyro_y < 30)
-	if request.sort.attr == "writing_time":
-		query_timeline = query_timeline.filter(models.Post.writing_time != None)
-		query_timeline = query_timeline.order_by(models.Post.writing_time)
-	if request.sort.attr == "velocity":
-		query_timeline = query_timeline.filter(models.Post.velocity != None)
-		query_timeline = query_timeline.order_by(models.Post.velocity)
-	if request.sort.attr == "elevation":
-		query_timeline = query_timeline.filter(models.Post.elevation != None)
-		query_timeline = query_timeline.order_by(models.Post.elevation)
+	if request.filter:
+		if request.filter.temperture:
+			query_timeline = query_timeline.filter(models.Post.tempurture != None)
+			if request.filter.temperture.max:
+				query_timeline = query_timeline.filter(models.Post.tempurture < request.filter.temperture.max)
+			if request.filter.temperture.min:
+				query_timeline = query_timeline.filter(models.Post.tempurture >= request.filter.temperture.min)
+		if request.filter.velocity:
+			query_timeline = query_timeline.filter(models.Post.velocity != None)
+			if request.filter.velocity.max:
+				query_timeline = query_timeline.filter(models.Post.velocity < request.filter.velocity.max)
+			if request.filter.velocity.min:
+				query_timeline = query_timeline.filter(models.Post.velocity >= request.filter.velocity.min)
+		if request.filter.elevation:
+			query_timeline = query_timeline.filter(models.Post.elevation != None)
+			if request.filter.elevation.max:
+				query_timeline = query_timeline.filter(models.Post.elevation < request.filter.elevation.max)
+			if request.filter.elevation.min:
+				query_timeline = query_timeline.filter(models.Post.elevation >= request.filter.elevation.min)
+		if request.filter.direction:
+			query_timeline = query_timeline.filter(models.Post.gyro_x != None).filter(models.Post.gyro_y != None).filter(models.Post.gyro_z != None)
+			if request.filter.direction == "up":
+				query_timeline = query_timeline.filter(-30 < models.Post.gyro_x, models.Post.gyro_x < 30, -30 < models.Post.gyro_y, models.Post.gyro_y < 30)
+			if request.filter.direction == "down":
+				query_timeline = query_timeline.filter(or_(150 < models.Post.gyro_x, models.Post.gyro_x < -150)).filter(-30 < models.Post.gyro_y, models.Post.gyro_y < 30)
+	if request.sort:
+		if request.sort.attr == "writing_time":
+			query_timeline = query_timeline.filter(models.Post.writing_time != None)
+			query_timeline = query_timeline.order_by(models.Post.writing_time)
+		if request.sort.attr == "velocity":
+			query_timeline = query_timeline.filter(models.Post.velocity != None)
+			query_timeline = query_timeline.order_by(models.Post.velocity)
+		if request.sort.attr == "elevation":
+			query_timeline = query_timeline.filter(models.Post.elevation != None)
+			query_timeline = query_timeline.order_by(models.Post.elevation)
+	else:
+		query_timeline = query_timeline.order_by(desc(models.Post.created_at))
 	
 	query_timeline = query_timeline.all()
 
 	query_timeline = list(map(Post.from_orm, query_timeline))
 
-	if request.sort.attr == "birthday":
-		query_timeline = list(filter(lambda x: x.user.date_of_birth != None, query_timeline))
-		query_timeline = sorted(query_timeline, key=lambda x:x.user.date_of_birth)
-	if request.sort.attr == "seen":
-		query_timeline = sorted(query_timeline, key=lambda x:len(x.seen_users))
+	if request.sort:
+		if request.sort.attr == "birthday":
+			query_timeline = list(filter(lambda x: x.user.date_of_birth != None, query_timeline))
+			query_timeline = sorted(query_timeline, key=lambda x:x.user.date_of_birth)
+		if request.sort.attr == "seen":
+			query_timeline = sorted(query_timeline, key=lambda x:len(x.seen_users))
 
-
-	if request.sort.order == "desc":
-		query_timeline.reverse()
+		if request.sort.order == "desc":
+			query_timeline.reverse()
 
 	return query_timeline
 
